5eb5ced1419c1f11c3645bd7e4f1f0a3839ac3bb
TomascpMarques <tomascpmarques@gmail.com>
2023-03-24 16:01:23 +0000

    base structure

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+from bs4 import BeautifulSoup
 
+fd = open('xml/example.xml', 'r')
 
-if __name__ == __main__:
-    ()
+xml_file = fd.read()
+
+soup = BeautifulSoup(xml_file, 'lxml')
+
+for tag in soup.findAll("item"):
+    # print(tag)
+    print(tag["name"])
+    print(tag.text)
+
+fd.close()
