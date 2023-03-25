190f0d1a21650966d4c71e90792bf0d94f1d6ccd
TomascpMarques <tomascpmarques@gmail.com>
2023-03-25 13:49:50 +0000

    Finished exercise

---
+++ ex2.py
@@ -0,0 +1,59 @@
+from bs4 import BeautifulSoup
+
+fd = open('xml/gamelist.xml', 'r')
+
+xml_file = fd.read()
+
+soup = BeautifulSoup(xml_file, features="lxml")
+
+
+class Game:
+    def __init__(self, name, desc, rating, releaseDate, developer, publisher, genre, players):
+        self.name = name
+        self. desc = desc
+        self. rating = rating
+        self. releaseDate = releaseDate
+        self. developer = developer
+        self. publisher = publisher
+        self. genre = genre
+        self. players = players
+
+
+Games = list[Game]
+
+games = Games()
+
+for tag in soup.findAll("game"):
+    for x in tag.find_all("name"):
+        tempGame = Game(
+            x.string, tag.desc.string, tag.rating.string, tag.releasedate.string,
+            tag.developer.string, tag.publisher.string, tag.genre.string, tag.players.string
+        )
+        games.append(tempGame)
+
+print(tempGame)
+
+gameXML = BeautifulSoup('<games></games>', features="xml")
+for game in games:
+    tempXML = BeautifulSoup('<game></game>', features="xml")
+    tag = tempXML.game
+    tag["playerCount"] = game.players
+    tag["released"] = game.releaseDate
+
+    nameTag = tempXML.new_tag("name")
+    nameTag.string = game.name
+
+    descTag = tempXML.new_tag("description")
+    descTag.string = game.desc[0:100] + '...'
+
+    genreTag = tempXML.new_tag("genre")
+    genreTag.string = game.genre
+
+    tag.append(nameTag)
+    tag.append(descTag)
+    tag.append(genreTag)
+    gameXML.games.append(tempXML)
+
+print(gameXML.prettify())
+
+fd.close()
