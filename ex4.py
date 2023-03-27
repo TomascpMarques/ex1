7cc8c70b48424ef407db9ecf89dd063c68b9e7aa
TomascpMarques <tomascpmarques@gmail.com>
2023-03-27 16:48:45 +0100

    yes

--- ex4.py
+++ ex4.py
@@ -1,3 +1,5 @@
+import json
+
 from bs4 import BeautifulSoup
 
 fd = open('xml/students.xml', 'r')
@@ -13,6 +15,10 @@ class Student:
         self.year = year
         self.testGrade = testGrade
 
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
+
 
 Students = dict[str, list[Student]]
 
@@ -21,7 +27,7 @@ students = Students()
 for tag in soup.findAll("student"):
     for name in tag.find_all("name"):
         tempStudent = Student(
-            name.string, tag.year.string, tag.testgrade
+            name.string, tag.year.string, tag.testgrade.contents[0]
         )
         year = tempStudent.year.strip(",.").lower()
         if year in students.keys():
@@ -55,5 +61,20 @@ for year in students.keys():
     studentsXML.students.append(tempXML)
 
 print(studentsXML.prettify())
+
+# TO JSON
+
+with open("json/ex4.json", mode='w') as fl:
+    fl.write("{")
+    for studentKey in students.keys():
+        fl.write('"'+studentKey+'": {')
+        # print(json.dumps(students[studentKey]))
+        for student in students[studentKey]:
+            fl.write(json.dumps(student.__dict__) + ',')
+            # print(student.__dict__)
+            # print(json.dumps(student.__dict__))
+        fl.write('}')
+    fl.write("}")
+
 
 fd.close()
